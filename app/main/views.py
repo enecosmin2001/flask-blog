f1708b077eeb00b581aeec9f23fe1c6abcaca86b
Cosmin Ene <enecosminmihai@outlook.com>
2022-10-03 19:23:30 +0300

    Intial project structure commit

---
+++ app/main/views.py
@@ -0,0 +1,36 @@
+from flask import current_app, redirect, render_template, session, url_for
+
+from .. import db
+from ..email import send_email
+from ..models import User
+from . import main
+from .forms import NameForm
+
+
+@main.route("/", methods=["GET", "POST"])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session["known"] = False
+            if current_app.config["FlaskBlog_ADMIN"]:
+                send_email(
+                    current_app.config["FlaskBlog_ADMIN"],
+                    "New User",
+                    "mail/new_user",
+                    user=user,
+                )
+        else:
+            session["known"] = True
+        session["name"] = form.name.data
+        return redirect(url_for(".index"))
+    return render_template(
+        "index.html",
+        form=form,
+        name=session.get("name"),
+        known=session.get("known", False),
+    )
