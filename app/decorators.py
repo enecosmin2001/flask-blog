4b37be4edce65ce4530a7c8d84eae8a7b560acab
Cosmin Ene <enecosminmihai@outlook.com>
2022-10-04 15:17:10 +0300

    Implement administrator level profile editor

--- app/decorators.py
+++ app/decorators.py
@@ -10,7 +10,7 @@ def permission_required(permission):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
-            if not current_user.can(Permission):
+            if not current_user.can(permission):
                 abort(403)
             return f(*args, **kwargs)
 
