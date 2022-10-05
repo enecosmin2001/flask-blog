1f25bfbd26efe983459ae11506f8555d3d36a5b7
Cosmin Ene <enecosminmihai@outlook.com>
2022-10-05 14:05:01 +0300

    Add markdown html for stylized blog posts

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,4 @@
+from flask_pagedown.fields import PageDownField
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, SelectField, StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length, Regexp, ValidationError
@@ -61,5 +62,5 @@ class EditProfileAdminForm(FlaskForm):
 
 
 class PostForm(FlaskForm):
-    body = TextAreaField("What's on your mind?", validators=[DataRequired()])
+    body = PageDownField("What's on your mind?", validators=[DataRequired()])
     submit = SubmitField("Post")
