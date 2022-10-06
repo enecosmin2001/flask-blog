d5ba23733f367d72a108a9ceabdcf4e7c4abb710
Cosmin Ene <enecosminmihai@outlook.com>
2022-10-06 12:48:34 +0300

    Add admin moderate comments functionality

--- app/main/views.py
+++ app/main/views.py
@@ -152,6 +152,42 @@ def post(id):
     )
 
 
+@main.route("/moderate")
+@permission_required(Permission.MODERATE)
+@login_required
+def moderate():
+    page = request.args.get("page", 1, type=int)
+    pagination = Comment.query.order_by(Comment.timestamp.desc()).paginate(
+        page,
+        per_page=current_app.config["FLASKBLOG_COMMENTS_PER_PAGE"],
+        error_out=False,
+    )
+    comments = pagination.items
+    return render_template(
+        "moderate.html", comments=comments, pagination=pagination, page=page
+    )
+
+
+@main.route("/moderate/enable/<int:id>")
+@permission_required(Permission.MODERATE)
+@login_required
+def moderate_enable(id):
+    comment = Comment.query.get_or_404(id)
+    comment.disabled = False
+    db.session.commit()
+    return redirect(url_for(".moderate", page=request.args.get("page,", 1, type=int)))
+
+
+@main.route("/moderate/disable/<int:id>")
+@permission_required(Permission.MODERATE)
+@login_required
+def moderate_disable(id):
+    comment = Comment.query.get_or_404(id)
+    comment.disabled = True
+    db.session.commit()
+    return redirect(url_for(".moderate", page=request.args.get("page,", 1, type=int)))
+
+
 @main.route("/edit/<int:id>", methods=["GET", "POST"])
 @login_required
 def edit(id):
