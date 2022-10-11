36a9c4bf9f498559e58314d77d419777145e5bb5
Cosmin Ene <enecosminmihai@outlook.com>
2022-10-11 14:47:01 +0300

    Test api user posting functionality

--- app/apiv1/posts.py
+++ app/apiv1/posts.py
@@ -1,11 +1,39 @@
-from flask import g, jsonify, request, url_for
+from flask import current_app, g, jsonify, request, url_for
 
-from app import db
-from app.models import Permission, Post
-
-from ..decorators import permission_required
+from .. import db
+from ..models import Permission, Post
 from . import apiv1
+from .decorators import permission_required
 from .errors import forbidden
+
+
+@apiv1.route("/posts/")
+def get_posts():
+    page = request.args.get("page", 1, type=int)
+    pagination = Post.query.paginate(
+        page, per_page=current_app.config["FLASKBLOG_POSTS_PER_PAGE"], error_out=False
+    )
+    posts = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for("apiv1.get_posts", page=page - 1)
+    next = None
+    if pagination.has_next:
+        next = url_for("apiv1.get_posts", page=page + 1)
+    return jsonify(
+        {
+            "posts": [post.to_json() for post in posts],
+            "prev": prev,
+            "next": next,
+            "count": pagination.total,
+        }
+    )
+
+
+@apiv1.route("/posts/<int:id>")
+def get_post(id):
+    post = Post.query.get_or_404(id)
+    return jsonify(post.to_json())
 
 
 @apiv1.route("/posts/", methods=["POST"])
@@ -15,8 +43,10 @@ def new_post():
     post.author = g.current_user
     db.session.add(post)
     db.session.commit()
-    return jsonify(
-        post.to_json(), 201, {"Location": url_for("apiv1.get_post", id=post.id)}
+    return (
+        jsonify(post.to_json()),
+        201,
+        {"Location": url_for("apiv1.get_post", id=post.id)},
     )
 
 
@@ -29,19 +59,4 @@ def edit_post(id):
     post.body = request.json.get("body", post.body)
     db.session.add(post)
     db.session.commit()
-    return jsonify(post.to_json())
-
-
-@apiv1.route("/posts/")
-def get_posts():
-    posts = Post.query.all()
-
-    [print(post.to_json()) for post in posts]
-
-    return jsonify({"posts": [post.to_json() for post in posts]})
-
-
-@apiv1.route("/posts/<int:id>")
-def get_post(id):
-    post = Post.query.get_or_404(id)
     return jsonify(post.to_json())
---
+++ tests/test_api.py
@@ -0,0 +1,69 @@
+import json
+import unittest
+from base64 import b64encode
+
+from flask import url_for
+
+from app import create_app, db
+from app.models import Role, User
+
+
+class APIV1TestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.app = create_app("testing")
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+        self.client = self.app.test_client(use_cookies=True)
+
+    def tearDown(self) -> None:
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def get_api_headers(self, username, password):
+        return {
+            "Authorization": "Basic "
+            + b64encode((username + ":" + password).encode("utf-8")).decode("utf-8"),
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        }
+
+    def test_no_auth(self):
+        response = self.client.get(
+            url_for("apiv1.get_posts"), content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 401)
+
+    def test_posts(self):
+        r = Role.query.filter_by(name="User").first()
+        self.assertIsNotNone(r)
+
+        u = User(email="john@test.com", password="cat", confirmed=True, role=r)
+        db.session.add(u)
+        db.session.commit()
+
+        # write a post
+        response = self.client.post(
+            url_for("apiv1.new_post"),
+            headers=self.get_api_headers("john@test.com", "cat"),
+            data=json.dumps({"body": "Body of the *blog* post"}),
+        )
+
+        self.assertEqual(response.status_code, 201)
+        url = response.headers.get("Location")
+        self.assertIsNotNone(url)
+
+        # get the new post
+        response = self.client.get(
+            url, headers=self.get_api_headers("john@test.com", "cat")
+        )
+
+        self.assertEqual(response.status_code, 200)
+        json_response = json.loads(response.get_data(as_text=True))
+        self.assertEqual("http://127.0.0.1" + json_response["url"], url)
+        self.assertEqual(json_response["body"], "Body of the *blog* post")
+        self.assertEqual(
+            json_response["body_html"], "<p>Body of the <em>blog</em> post</p>"
+        )
