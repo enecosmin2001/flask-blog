78fa559cd16026af93d8a30a75c1229c3c5f8fa9
Cosmin Ene <enecosminmihai@outlook.com>
2022-10-12 14:16:43 +0300

    Configure smt server for heroku

--- config.py
+++ config.py
@@ -10,8 +10,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "hard to guess string"
     MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.googlemail.com")
-    MAIL_PORT = int(os.environ.get("MAIL_PORT", "587"))
-    MAIL_TLS = os.environ.get("MAIL_USE_TLS", "true").lower() in ["true", "on", "1"]
+    MAIL_PORT = int(os.environ.get("MAIL_PORT", "465"))
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     FLASKBLOG_MAIL_SUBJECT_PREFIX = os.environ.get("FLASKBLOG_MAIL_SUBJECT_PREFIX")
